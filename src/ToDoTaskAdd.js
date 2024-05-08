646d0bea36e25eeb22a00ea8189d31186207b7ff
Andrej Chakrygin <imovitae@mail.ru>
2024-05-08 22:23:03 +0300

    Multiple screens++

--- src/ToDoTaskAdd.js
+++ src/ToDoTaskAdd.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,NavLink } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {todoAdd} from './actions'
 class ToDoTaskAddInner extends React.Component{
 	constructor(props){
 		super(props)
@@ -42,20 +44,23 @@ class ToDoTaskAddInner extends React.Component{
 			return res.json();
 		}).then((data)=>{
 			
-			this.props.onTaskAdd(data);
-			this.props.history('/');
+			this.props.dispatch(todoAdd(data._id,data.name,data.description));
+			this.props.history('/')
 			
 		});
 	}
 	
 	render(){
 return (
+		<div className="Add">
+		<NavLink to='/'>Back to list</NavLink>
 		<form onSubmit={this.onAddFormsSubmit}>
 			<input type ="text" value={this.state.name} onChange={this.onNameChange} placeholder="Name"/>
 			<input type ="text" value={this.state.description} onChange={this.onDescriptionChange} placeholder="Description"/>
 			<input type ="submit" value="Add"/>
 			
 		</form>
+		</div>
 		)
 	}
 }
@@ -64,4 +69,4 @@ const ToDoTaskAdd=(props)=>{
 	<ToDoTaskAddInner {...props} history={useNavigate()}/>
 	)
 }
-export default ToDoTaskAdd;
+export default connect()(ToDoTaskAdd);
